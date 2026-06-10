701c27034a63d12ed7feba2a7310ddc9e7418500
Admin <Admin@DESKTOP-8URQBFL>
2026-06-10 22:30:28 +0500

    Server closing problem DONE

--- Client/main.cpp
+++ Client/main.cpp
@@ -186,8 +186,12 @@ VOID Recieve(SOCKET connect_socket)
 		CHAR szError[256] = {};
 		cout << FormatLastError(dwError, szError) << endl;*/
 		if (iResult > 0) cout << recvbuffer << "(" << iResult << " Bytes)" << endl;
-		//else if (result == 0) cout << "Connection closed" << endl;
-		else cout << FormatLastError(WSAGetLastError(), szError) << endl;
+		else if (iResult == 0) cout << "Connection closed" << endl;
+		else 
+		{
+			cout << FormatLastError(WSAGetLastError(), szError) << endl;
+			break;
+		}
 		//cout << "Receive failed\t" << WSAGetLastError() << endl;
 	} while (strcmp(recvbuffer, DECLINE_MESSAGE) != 0);
 	if (strcmp(recvbuffer, DECLINE_MESSAGE) == 0)
